53302fbbb6cf7e6f499c5fb8124102cc3cfda332
Jonathan <jonathan.23.23@icloud.com>
2025-10-02 20:37:24 -0300

    se sube estructura base del home y enlace

--- Levelup-gamer/src/App.jsx
+++ Levelup-gamer/src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from '../pages/HomePage/HomePage.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Routes>
+        <Route path="/Home" element={<HomePage />} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
