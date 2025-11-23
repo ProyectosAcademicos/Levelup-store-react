0ff94d60575686ff73f828a04e3c152c62cc1c91
Diana3180chav <dia.chavez@duocuc.cl>
2025-11-23 14:00:23 -0300

    cambios en el headerCliente y agregar Swagger en dependencias

--- frontend/src/components/Header/HeaderCliente/index.jsx
+++ frontend/src/components/Header/HeaderCliente/index.jsx
@@ -33,6 +33,16 @@ const HeaderCliente = () => {
     navigate("/home"); // ← redirige al home
   };
 
+  const goToLogin = () => {
+    navigate("/login");
+  };
+
+  const goToRegister = () => {
+    navigate("/register");
+  
+  };
+
+
 
   const [menuOpen, setMenuOpen] = useState(false);
 
