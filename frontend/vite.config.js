0aa3ce0a2afbf628f919eab603407beb6603ca4e
Cesar Daniel (xDaniel-R) <ce.caceres@duocuc.cl>
2025-11-27 12:14:51 -0300

    actualizando archivos dockerfile.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,29 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Redirige todas las peticiones que empiecen con /api
       '/api': {
-        target: 'http://localhost:8080', // backend Spring Boot
-        changeOrigin: true, // Necesario para la redirección
-        secure: false,      // backend corre en http
+        target: 'http://backend:8080',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       // Redirige todas las peticiones que empiecen con /api
+//       '/api': {
+//         target: 'http://localhost:8080', // backend Spring Boot
+//         changeOrigin: true, // Necesario para la redirección
+//         secure: false,      // backend corre en http
+//       }
+//     }
+//   }
+// })
