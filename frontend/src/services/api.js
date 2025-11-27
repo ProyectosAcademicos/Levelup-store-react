4e1431f88dfb32eb36c353fe04812736b748bbf4
Cesar Daniel (xDaniel-R) <ce.caceres@duocuc.cl>
2025-11-27 14:27:37 -0300

    Actualizando api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -11,7 +11,7 @@
 
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 // ============= AUTENTICACIÓN =============
 export const loginUser = async (credentials) => {
