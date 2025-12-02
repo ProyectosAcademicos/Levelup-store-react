a5ac49d8d908af020120b0cfe7acbd982ff2039b
Jonathan <jonathan.23.23@icloud.com>
2025-12-02 00:24:02 -0300

    se agrega ruta elastica

--- frontend/src/components/RegisterContenido.jsx/RegisterContenido.jsx
+++ frontend/src/components/RegisterContenido.jsx/RegisterContenido.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Container, Row, Col, Card, Form, Button, Alert } from "react-bootstrap";
 import styles from "./RegisterContenido.module.css";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const API_BASE = import.meta.env.VITE_API_URL || "http://98.95.1.187:8080";
 const api = axios.create({ baseURL: API_BASE });
 
 const RegisterPage = () => {
